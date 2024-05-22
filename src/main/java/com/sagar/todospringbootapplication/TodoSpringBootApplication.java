674607a3dec1e0654869322ec79ededf56f178da
saptha sagar <sapthasagar533@gmail.com>
2024-05-22 16:26:27 +0530

    Initial commit

---
+++ src/main/java/com/sagar/todospringbootapplication/TodoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.todospringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/todospringbootapplication/TodoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.todospringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
